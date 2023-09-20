a8676a1f815042026f9b5ae80a97c6ec62206fad
khartson <khartson>
2023-09-20 18:18:28 -0400

    Edited form fields + code IDs

--- ComPort/Form1.Designer.cs
+++ ComPort/Form1.Designer.cs
@@ -29,22 +29,22 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.comboBox3 = new System.Windows.Forms.ComboBox();
-            this.comboBox4 = new System.Windows.Forms.ComboBox();
-            this.comboBox5 = new System.Windows.Forms.ComboBox();
-            this.comboBox6 = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboxParityBits = new System.Windows.Forms.ComboBox();
+            this.cboxStopBits = new System.Windows.Forms.ComboBox();
+            this.cboxDataBits = new System.Windows.Forms.ComboBox();
+            this.cboxBaudRate = new System.Windows.Forms.ComboBox();
+            this.cboxComPort = new System.Windows.Forms.ComboBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.button3 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnSendConfig = new System.Windows.Forms.Button();
+            this.tboxDataOut = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
@@ -56,95 +56,17 @@
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.comboBox2);
-            this.groupBox1.Controls.Add(this.comboBox3);
-            this.groupBox1.Controls.Add(this.comboBox4);
-            this.groupBox1.Controls.Add(this.comboBox5);
-            this.groupBox1.Controls.Add(this.comboBox6);
+            this.groupBox1.Controls.Add(this.cboxParityBits);
+            this.groupBox1.Controls.Add(this.cboxStopBits);
+            this.groupBox1.Controls.Add(this.cboxDataBits);
+            this.groupBox1.Controls.Add(this.cboxBaudRate);
+            this.groupBox1.Controls.Add(this.cboxComPort);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(256, 161);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "COM PORT Control";
-            // 
-            // comboBox2
-            // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(89, 126);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(161, 21);
-            this.comboBox2.TabIndex = 2;
-            // 
-            // comboBox3
-            // 
-            this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(89, 100);
-            this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(161, 21);
-            this.comboBox3.TabIndex = 3;
-            // 
-            // comboBox4
-            // 
-            this.comboBox4.FormattingEnabled = true;
-            this.comboBox4.Location = new System.Drawing.Point(89, 73);
-            this.comboBox4.Name = "comboBox4";
-            this.comboBox4.Size = new System.Drawing.Size(161, 21);
-            this.comboBox4.TabIndex = 4;
-            // 
-            // comboBox5
-            // 
-            this.comboBox5.FormattingEnabled = true;
-            this.comboBox5.Location = new System.Drawing.Point(89, 46);
-            this.comboBox5.Name = "comboBox5";
-            this.comboBox5.Size = new System.Drawing.Size(161, 21);
-            this.comboBox5.TabIndex = 5;
-            // 
-            // comboBox6
-            // 
-            this.comboBox6.FormattingEnabled = true;
-            this.comboBox6.Location = new System.Drawing.Point(89, 19);
-            this.comboBox6.Name = "comboBox6";
-            this.comboBox6.Size = new System.Drawing.Size(161, 21);
-            this.comboBox6.TabIndex = 6;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(7, 23);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 13);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "COM PORT";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(7, 50);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(69, 13);
-            this.label2.TabIndex = 8;
-            this.label2.Text = "BAUD RATE";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(7, 77);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(63, 13);
-            this.label3.TabIndex = 9;
-            this.label3.Text = "DATA BITS";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(7, 104);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(63, 13);
-            this.label4.TabIndex = 10;
-            this.label4.Text = "STOP BITS";
-            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // label5
             // 
@@ -156,35 +78,95 @@
             this.label5.Text = "PARITY BITS";
             this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "STOP BITS";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "DATA BITS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "BAUD RATE";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "COM PORT";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // cboxParityBits
+            // 
+            this.cboxParityBits.FormattingEnabled = true;
+            this.cboxParityBits.Location = new System.Drawing.Point(89, 126);
+            this.cboxParityBits.Name = "cboxParityBits";
+            this.cboxParityBits.Size = new System.Drawing.Size(161, 21);
+            this.cboxParityBits.TabIndex = 2;
+            // 
+            // cboxStopBits
+            // 
+            this.cboxStopBits.FormattingEnabled = true;
+            this.cboxStopBits.Location = new System.Drawing.Point(89, 100);
+            this.cboxStopBits.Name = "cboxStopBits";
+            this.cboxStopBits.Size = new System.Drawing.Size(161, 21);
+            this.cboxStopBits.TabIndex = 3;
+            // 
+            // cboxDataBits
+            // 
+            this.cboxDataBits.FormattingEnabled = true;
+            this.cboxDataBits.Location = new System.Drawing.Point(89, 73);
+            this.cboxDataBits.Name = "cboxDataBits";
+            this.cboxDataBits.Size = new System.Drawing.Size(161, 21);
+            this.cboxDataBits.TabIndex = 4;
+            // 
+            // cboxBaudRate
+            // 
+            this.cboxBaudRate.FormattingEnabled = true;
+            this.cboxBaudRate.Location = new System.Drawing.Point(89, 46);
+            this.cboxBaudRate.Name = "cboxBaudRate";
+            this.cboxBaudRate.Size = new System.Drawing.Size(161, 21);
+            this.cboxBaudRate.TabIndex = 5;
+            // 
+            // cboxComPort
+            // 
+            this.cboxComPort.FormattingEnabled = true;
+            this.cboxComPort.Location = new System.Drawing.Point(89, 19);
+            this.cboxComPort.Name = "cboxComPort";
+            this.cboxComPort.Size = new System.Drawing.Size(161, 21);
+            this.cboxComPort.TabIndex = 6;
+            // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.progressBar1);
-            this.groupBox2.Controls.Add(this.button2);
-            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.btnClose);
+            this.groupBox2.Controls.Add(this.btnOpen);
             this.groupBox2.Location = new System.Drawing.Point(12, 179);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(175, 104);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(10, 19);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(91, 19);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
             // 
             // progressBar1
             // 
@@ -193,31 +175,50 @@
             this.progressBar1.Size = new System.Drawing.Size(156, 23);
             this.progressBar1.TabIndex = 2;
             // 
-            // button3
+            // btnClose
             // 
-            this.button3.Location = new System.Drawing.Point(193, 184);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 94);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnClose.Location = new System.Drawing.Point(91, 19);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "CLOSE";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.button2_Click);
             // 
-            // textBox1
+            // btnOpen
             // 
-            this.textBox1.Location = new System.Drawing.Point(274, 12);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(299, 271);
-            this.textBox1.TabIndex = 3;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.btnOpen.Location = new System.Drawing.Point(10, 19);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "OPEN";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            // 
+            // btnSendConfig
+            // 
+            this.btnSendConfig.Location = new System.Drawing.Point(193, 184);
+            this.btnSendConfig.Name = "btnSendConfig";
+            this.btnSendConfig.Size = new System.Drawing.Size(75, 94);
+            this.btnSendConfig.TabIndex = 2;
+            this.btnSendConfig.Text = "Send Config";
+            this.btnSendConfig.UseVisualStyleBackColor = true;
+            // 
+            // tboxDataOut
+            // 
+            this.tboxDataOut.Location = new System.Drawing.Point(274, 12);
+            this.tboxDataOut.Multiline = true;
+            this.tboxDataOut.Name = "tboxDataOut";
+            this.tboxDataOut.Size = new System.Drawing.Size(299, 271);
+            this.tboxDataOut.TabIndex = 3;
+            this.tboxDataOut.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(589, 310);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button3);
+            this.Controls.Add(this.tboxDataOut);
+            this.Controls.Add(this.btnSendConfig);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
@@ -239,17 +240,17 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.ComboBox comboBox3;
-        private System.Windows.Forms.ComboBox comboBox4;
-        private System.Windows.Forms.ComboBox comboBox5;
-        private System.Windows.Forms.ComboBox comboBox6;
+        private System.Windows.Forms.ComboBox cboxParityBits;
+        private System.Windows.Forms.ComboBox cboxStopBits;
+        private System.Windows.Forms.ComboBox cboxDataBits;
+        private System.Windows.Forms.ComboBox cboxBaudRate;
+        private System.Windows.Forms.ComboBox cboxComPort;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.ProgressBar progressBar1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnSendConfig;
+        private System.Windows.Forms.TextBox tboxDataOut;
     }
 }
 
--- ComPort/Form1.cs
+++ ComPort/Form1.cs
@@ -46,5 +46,10 @@ namespace ComPort
         {
 
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
